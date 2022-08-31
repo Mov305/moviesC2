59e015da9189d3f46b0102f7cd71d4d9eefcd48e
Mov305 <mov3three2001@gmail.com>
2022-08-31 20:12:12 +0200

    added api req

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,9 +3,7 @@ import '../css/style.css'
 import '../css/index.css'
 import movies from './modules/movies.js'
 import {setUi} from './modules/usages.js'
-import myShowsPopup from './modules/popup.js';
 
-myShowsPopup();
 setUi()
 movies.fetchMovies();
 
--- src/javascript/modules/movies.js
+++ src/javascript/modules/movies.js
@@ -1,15 +1,28 @@
 class Movies {
   BaseURL = 'https://api.tvmaze.com/search/shows?q=';
+  invURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ikULMDdkefW4ekGDByKD/'
 
   async fetchMovies(search = 'marvel', page = '1') {
     const api = `${this.BaseURL}${search}&page=${page}`;
     try {
       const res = await fetch(api);
       const movies = await res.json();
-      return movies;
-      this.render(movies);
+      const likes = await this.fetchInv('likes');
+      this.render(movies,likes);
+
     } catch (error) {
-      console.log(error);
+      console.log(error,'errorapi1');
+    }
+  }
+
+  async fetchInv(type) {
+    const api = `${this.invURL}${type}`
+    try {
+      const res = await fetch(api);
+      const data = await res.json();
+      return data;
+    }catch(error){
+      console.log(error,'error,api2')
     }
   }
 
@@ -29,8 +42,57 @@ class Movies {
       li.textContent = genre;
       movieGenre.appendChild(li);
     });
-    main.classList.remove('opacity-20')
+    main.classList.remove('opacity-20');
+    viewMovie.onclick = () => {
+      this.myShowsPopup(movie);
+      popUp.classList.remove('hidden');
+      closePopup.onclick = ()=> popUp.classList.add('hidden');
+    };
+
   }
+
+  myShowsPopup = ({ show }) => {
+    popUp.innerHTML = `<div class="popupBackground">
+  <section class="popUpWindow">
+    <img src="${show.image?.original}" alt="#" class="popupImg">
+    <div id='closePopup' class="closeIcon">
+    <img src="https://img.icons8.com/color/48/000000/delete-sign--v1.png"/>
+    </div>
+    <div class="showContent">
+    <div class="showInfo">
+    <h2 class="showTitle">${show.name}</h2>
+    <p id="mySum">${show.summary}</p>
+    <div class="moreInfo">
+        <p class="showRate rNumber"><img src="https://img.icons8.com/external-flaticons-flat-circular-flat-icons/64/000000/external-rate-customer-feedback-flaticons-flat-circular-flat-icons.png" class="rate"/>Rating : ${show.rating.average}</p>
+        <p class="showRate"><img src="https://img.icons8.com/external-flaticons-flat-circular-flat-icons/64/000000/external-live-tv-live-streaming-flaticons-flat-circular-flat-icons.png" class="rate"/>Genres : ${show.genres}</p>
+        <p class="showRate"><img src="https://img.icons8.com/external-flaticons-flat-circular-flat-icons/64/000000/external-on-air-live-streaming-flaticons-flat-circular-flat-icons.png" class="rate"/>Language : ${show.language}</p>
+        <p class="showDates hideDesk"><img src="https://img.icons8.com/external-flaticons-flat-circular-flat-icons/64/000000/external-estimate-engineering-flaticons-flat-circular-flat-icons.png" class="rate"/>
+        Premiered : ${show.premiered} - Ended : ${show.ended}</p>
+    </div>
+    <p class="showDates hideMob"><img src="https://img.icons8.com/external-flaticons-flat-circular-flat-icons/64/000000/external-estimate-engineering-flaticons-flat-circular-flat-icons.png" class="rate"/>
+      Premiered : ${show.premiered} - Ended : ${show.ended}</p>
+    </div>
+
+    <div class"commentSection">
+      <h3 class="commentTitle">Comments</h3>
+      <ul id="myComments">
+      <li>hello</li>
+      <li>hello</li>
+      </ul>
+    </div>
+
+    <div class="createComment">
+      <h3 id="inputTitle">Add a Comment</h3>
+      <form action="#" method="POST" id="inputSection">
+        <input id="myUser"type="text" placeholder="Your name" required>
+        <textarea id="myInputComment" name="user_name" id="user-message" maxlength="500" placeholder="Your insights" required></textarea>
+        <input type="submit" value="Comment" id="commentBtn">
+      </form>
+    </div>
+   </div>
+  </section>
+</div>`;
+  };
 
   render(data) {
     this.renderBackground(data[0]);
@@ -42,7 +104,10 @@ class Movies {
       const span2 = document.createElement('span');
       const span3 = document.createElement('span');
       const p = document.createElement('p');
-      li.onclick = () => {main.classList.add('opacity-20');setTimeout(()=>this.renderBackground(movie),300)};
+      li.onclick = () => {
+        main.classList.add('opacity-20');
+        setTimeout(() => this.renderBackground(movie), 300);
+      };
       span.textContent = movie.show.status;
       p.textContent = movie.show.name;
       span2.textContent = movie.show.rating.average ? '⭐' + movie.show.rating.average : 'No Rate';
